33e04166db1c144b7df5da877e56c19d824b2d93
Aviram <aviram@orbs.com>
2020-08-11 12:08:11 +0300

    Updates

---
+++ precache-manifest.5197bac3960165985da53c3622f93581.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "734da6c426012bd2e60ad51cb5b31007",
+    "url": "https://orbs-network.github.io/v2-event-dashboard/index.html"
+  },
+  {
+    "revision": "c38593b4dcb95e40369f",
+    "url": "https://orbs-network.github.io/v2-event-dashboard/static/css/main.d4b27514.chunk.css"
+  },
+  {
+    "revision": "ba02a44b285f68e48c69",
+    "url": "https://orbs-network.github.io/v2-event-dashboard/static/js/2.04847cfc.chunk.js"
+  },
+  {
+    "revision": "775071e7908c30b629946039840806b0",
+    "url": "https://orbs-network.github.io/v2-event-dashboard/static/js/2.04847cfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c38593b4dcb95e40369f",
+    "url": "https://orbs-network.github.io/v2-event-dashboard/static/js/main.119706d6.chunk.js"
+  },
+  {
+    "revision": "9a37b2f01e2053dfe74b",
+    "url": "https://orbs-network.github.io/v2-event-dashboard/static/js/runtime-main.7473390d.js"
+  }
+]);
