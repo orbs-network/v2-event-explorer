42b6cc41e87739647b0d1601e7566b9270e0bdc8
Aviram <aviram@orbs.com>
2020-08-13 10:53:38 +0300

    Updates

---
+++ precache-manifest.637bfc14e8caba26ece08ccc92fb5ff8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a8da843949656f467b99abf20160f6",
+    "url": "https://orbs-network.github.io/v2-event-dashboard/index.html"
+  },
+  {
+    "revision": "10070d53e52a07b231cb",
+    "url": "https://orbs-network.github.io/v2-event-dashboard/static/css/main.d4b27514.chunk.css"
+  },
+  {
+    "revision": "ba02a44b285f68e48c69",
+    "url": "https://orbs-network.github.io/v2-event-dashboard/static/js/2.04847cfc.chunk.js"
+  },
+  {
+    "revision": "775071e7908c30b629946039840806b0",
+    "url": "https://orbs-network.github.io/v2-event-dashboard/static/js/2.04847cfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10070d53e52a07b231cb",
+    "url": "https://orbs-network.github.io/v2-event-dashboard/static/js/main.c7c12206.chunk.js"
+  },
+  {
+    "revision": "9a37b2f01e2053dfe74b",
+    "url": "https://orbs-network.github.io/v2-event-dashboard/static/js/runtime-main.7473390d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d1e34f09cfcb4a490352eeaff392ecd1.js"
+  "https://orbs-network.github.io/v2-event-dashboard/precache-manifest.637bfc14e8caba26ece08ccc92fb5ff8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://orbs-network.github.io/v2-event-dashboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
